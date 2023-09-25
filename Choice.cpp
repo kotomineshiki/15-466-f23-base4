9efd3fe615ee5d4a9ad751b1a54f6e30ccde450c
RubysWork <yuew6@anddrew.cmu.edu>
2023-09-24 22:36:29 -0400

    test

--- Choice.cpp
+++ Choice.cpp
@@ -8,6 +8,26 @@ void Choice::TestChoice(){
 }
 
 void ChoiceTable::ReadCSVFile(std::string dataPath){
+<<<<<<< HEAD
  //   std::ifstream input;
  //   input.open(dataPath,)
+=======
+    std::ifstream input;
+    input.open(dataPath,std::ios::in);
+    std::cout<<"StartReading"+dataPath<<std::endl;
+    if(!input.is_open()){
+        std::cout<<"Read Error"<<std::endl;
+        return;
+    }
+    std::string choiceString;
+    std::getline(input,choiceString);//abandan first string because they are Header
+    while(std::getline(input,choiceString)){
+        std::cout<<choiceString<<std::endl;
+        Choice Choice(choiceString);
+
+    }
+    input.close();
+    std::cout<<"Done"<<std::endl;
+    return;
+>>>>>>> parent of a577a20 (Upload Choice Fix)
 }
